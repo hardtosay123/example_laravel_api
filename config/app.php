a61771fdab6ec2b434935872bf6e632c606e50a8
hardtosay123 <zikeongchong@hotmail.com>
2021-10-09 19:31:45 +0800

    commit last change

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', base64_decode('NBhQKcmh4AyQmhx5O6TScnmIEo6G9WG1nRf9SsmzIZ0=')),
 
     'cipher' => 'AES-256-CBC',
 
